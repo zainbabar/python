d173629de01f49ae6e36d21e199e6a08b3486ed4
zainbabar <49132055+zainbabar@users.noreply.github.com>
2019-08-24 09:56:15 -0700

    escape

--- escape/listings/listing4-2.py
+++ escape/listings/listing4-2.py
@@ -13,8 +13,8 @@ HEIGHT = 800
 
 #PLAYER variables
 PLAYER_NAME = "Zain" # change this to your name!
-FRIEND1_NAME = "Zair aka dumb kid" # change this to a friend's name!
-FRIEND2_NAME = "old fat man" # change this to another friend's name!
+FRIEND1_NAME = "F1" # change this to a friend's name!
+FRIEND2_NAME = "F2" # change this to another friend's name!
 current_room = 31 # start room = 31
 
 top_left_x = 100
@@ -80,63 +80,62 @@ def get_floor_type():
         else:
                 return 0 # tiled floor
 
-def generate_map()
+def generate_map():
 # This function makes map for current room,
 # using room data, scenery data and prop data.
+    global room_map, room_width, room_height, room_name, hazard_map
+    global top_left_x, top_left_y, wall_transparency_frame
+    room_data = GAME_MAP[current_room]
+    room_name = room_data[0]
+    room_height = room_data[1]
+    room_width = room_data[2]
 
-global room_map, room_height, room_width, room_name, hazard_map
-global top_left_x, top_left_y, wall_transparency_frame
-room_data = GAME_MAP[current_room]
-room_name = room_data[0]
-room_height = room_data[1]
-room_width = room_data[2]
+    floor_type = get_floor_type()
+    if current_room in range(1, 21):
+            bottom_edge = 2 #soil
+            side_edge = 2 #soil
+    if current_room in range (21, 26):
+            bottom_edge = 1 #wall
+            side_edge = 2 #soil
+    if current_room > 25:
+            bottom_edge = 1 #wall
+            side_edge = 1 #wall
 
-floor_type = get_floor_type()
-if current_room in range(1, 21):
-        bottom_edge = 2 #soil
-        side_edge = 2 #soil
-if current_room in range (21, 26)
-        bottom_edge = 1 #wall
-        side_edge = 2 #soil
-if current_room > 25:
-        bottom_edge = 1 #wall
-        side_edge = 1 #wall
+    # create top line of room map
+    room_map=[[side_edge] * room_width]
+    # Add any middle lines of room map (wall, floor to fill width, wall).
+    for y in range(room_height - 2):
+            room_map.append([side_edge]
+                    + [floor_type]*(room_width - 2) + [side_edge])
+    # Add bottom line of room map,
+    room_map.append([bottom_edge] * room_width)
 
-# create top line of room map
-room_map=[[side_edge] * room_width]
-# Add any middle lines of room map (wall, floor to fill width, wall).
-for y in range(room_height - 2):
-        room_map.append([side_edge]
-                + [floor_type]*(room_width - 2) + [side_edge])
-# Add bottom line of room map,
-room_map.append([bottom_edge] * room_width)
+    # Add doorways.
+    middle_row = int(room_height / 2)
+    middle_column = int(room_width / 2)
 
-# Add doorways.
-middle_row = int(room_height / 2)
-middle_column = int(room_width / 2)
+    if room_data[4]: # If exit at right of this room
+            room_map[middle_row][room_width - 1] = floor_type
+            room_map[middle_row+1][room_width - 1] = floor_type
+            room_map[middle_row-1][room_width - 1] = floor_type
 
-if room_data[4]: # If exit at right of this room
-        room_map[middle_row][room_width - 1] = floor_type
-        room_map[middle_row+1][room_width - 1] = floor_type
-        room_map[middle_row-1][room_width - 1] = floor_type
+    if current_room % MAP_WIDTH != 1: # If room is not on left of map
+            room_to_left = GAME_MAP[current_room - 1]
+            # If room on the left has a right exit, add left exit in this room
+            if room_to_left[4]:
+                    room_map[middle_row][0] = floor_type
+                    room_map[middle_row][ + 1][0] = floor_type
+                    room_map[middle_row][ - 1][0] = floor_type
 
-if current_room % MAP_WIDTH != 1: # If room is not on left of map
-        room_to_left = GAME_MAP[current_room - 1]
-        # If room on the left has a right exit, add left exit in this room
-        if room_to_left[4]:
-                room_map[middle_row][0] = floor_type
-                room_map[middle_row][ + 1][0] = floor_type
-                room_map[middle_row][ - 1][0] = floor_type
+    if room_data[3]: # If exit at top of this room
+            room_map[0][middle_column] = floor_type
+            room_map[0][middle_column + 1] = floor_type
+            room_map[0][middle_column - 1] = floor_type
 
-if room_data[3]: # If exit at top of this room
-        room_map[0][middle_column] = floor_type
-        room_map[0][middle_column + 1] = floor_type
-        room_map[0][middle_column - 1] = floor_type
-
-if current_room <= MAP_SIZE - MAP_WIDTH: # If room is not on bottom row
-        room_below = GAME_MAP[current_room+MAP_WIDTH]
-        #IF room below has a top exit add exit at the bottom of this one 
-        if room_below[3]:
-                room_map[room_height-1][middle_column] = floor_type
-                room_map[room_height-1][middle_column + 1] = floor_type
-                room_map[room_height-1][middle_column - 1] = floor_type
+    if current_room <= MAP_SIZE - MAP_WIDTH: # If room is not on bottom row
+            room_below = GAME_MAP[current_room+MAP_WIDTH]
+            #IF room below has a top exit add exit at the bottom of this one 
+            if room_below[3]:
+                    room_map[room_height-1][middle_column] = floor_type
+                    room_map[room_height-1][middle_column + 1] = floor_type
+                    room_map[room_height-1][middle_column - 1] = floor_type
